d6a6ef452350ae69cb02b1635b1a3fe1246da8a0
kuyt1992 <51324232+kuyt1992@users.noreply.github.com>
2021-05-16 21:01:55 +0900

    feat: ユーザーカード作成

--- src/components/pages/UserMenagement.tsx
+++ src/components/pages/UserMenagement.tsx
@@ -1,5 +1,18 @@
+import { Box, Stack, Wrap, WrapItem, Image, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { UserCard } from "../organisms/user/UserCard";
+
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページ</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="ハル"
+          fullName="Kaito Tada"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
