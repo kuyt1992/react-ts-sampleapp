b7a32189a14be2ab2509ded56ee1d59dd7144a5f
kuyt1992 <51324232+kuyt1992@users.noreply.github.com>
2021-05-16 21:53:30 +0900

    feat: ユーザー一覧ページ作成

--- src/components/pages/UserMenagement.tsx
+++ src/components/pages/UserMenagement.tsx
@@ -1,18 +1,33 @@
-import { Box, Stack, Wrap, WrapItem, Image, Text } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { Wrap, WrapItem, Spinner, Center } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="ハル"
-          fullName="Kaito Tada"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
